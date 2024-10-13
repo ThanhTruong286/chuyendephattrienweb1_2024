c17370062c94b7b853c22cb0b440daf24bd355e1
ThanhTruong286 <caot43069@gmail.com>
2024-10-13 19:40:25 +0700

    add lab 4

---
+++ front/3105/index.php
@@ -0,0 +1,184 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Demo Digital</title>
+    <link rel="stylesheet" href="./assets/css/style.css">
+    <link rel="stylesheet" href="./assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"
+        integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" />
+    <link rel="stylesheet/less" type="text/css" href="./assets/less/3105.less" />
+    <script src="https://cdn.jsdelivr.net/npm/less@4.1.1"></script>
+
+</head>
+
+<body>
+
+    <footer id="footer">
+        <div class="footer-container">
+            <div class="container">
+                <div class="footer-background">
+                    <div class="row">
+                        <div class="col-md-9 col-xs-12">
+                            <h3 class="h3-title">showrooms system</h3>
+                            <div class="container" style="padding: 0;">
+                                <div class="row">
+                                    <li class="showroom-item col-4">
+                                        <div class="showroom-content">
+                                            <h4 class="showroom-content-title">
+                                                Arizona
+                                            </h4>
+                                            <div class="showroom-content-description">
+                                                <p>Address: Puffin street 12345 Puffinville <br>
+                                                    Phone: (123) 4567889 <br>
+                                                    Opening hours: 8:00 to 21:00 (all days)</p>
+                                                <div class="btn-color"><a href="#">View on map </a> <i
+                                                        class="fa-solid fa-angles-right fa-2xs"></i></div>
+                                            </div>
+                                        </div>
+                                    </li>
+                                    <li class="showroom-item col-4">
+                                        <div class="showroom-content">
+                                            <h4 class="showroom-content-title">
+                                                California
+                                            </h4>
+                                            <div class="showroom-content-description">
+                                                <p>Address: Puffin street 12345 Puffinville <br>
+                                                    Phone: (123) 4567889 <br>
+                                                    Opening hours: 8:00 to 21:00 (all days)</p>
+                                                <div class="btn-color"><a href="#">View on map </a> <i
+                                                        class="fa-solid fa-angles-right fa-2xs"></i></div>
+                                            </div>
+                                        </div>
+                                    </li>
+                                    <li class="showroom-item col-4">
+                                        <div class="showroom-content">
+                                            <h4 class="showroom-content-title">
+                                                Michigan
+                                            </h4>
+                                            <div class="showroom-content-description">
+                                                <p>Address: Puffin street 12345 Puffinville <br>
+                                                    Phone: (123) 4567889 <br>
+                                                    Opening hours: 8:00 to 21:00 (all days)</p>
+                                                <div class="btn-color"><a href="#">View on map </a> <i
+                                                        class="fa-solid fa-angles-right fa-2xs"></i></div>
+                                            </div>
+                                        </div>
+                                    </li>
+                                    <li class="showroom-item col-4">
+                                        <div class="showroom-content">
+                                            <h4 class="showroom-content-title">
+                                                Oregon
+                                            </h4>
+                                            <div class="showroom-content-description">
+                                                <p>Address: Puffin street 12345 Puffinville <br>
+                                                    Phone: (123) 4567889 <br>
+                                                    Opening hours: 8:00 to 21:00 (all days)</p>
+                                                <div class="btn-color"><a href="#">View on map </a> <i
+                                                        class="fa-solid fa-angles-right fa-2xs"></i></div>
+                                            </div>
+                                        </div>
+                                    </li>
+                                    <li class="showroom-item col-4">
+                                        <div class="showroom-content">
+                                            <h4 class="showroom-content-title">
+                                                Texas
+                                            </h4>
+                                            <div class="showroom-content-description">
+                                                <p>Address: Puffin street 12345 Puffinville <br>
+                                                    Phone: (123) 4567889 <br>
+                                                    Opening hours: 8:00 to 21:00 (all days)</p>
+                                                <div class="btn-color"><a href="#">View on map </a> <i
+                                                        class="fa-solid fa-angles-right fa-2xs"></i></div>
+                                            </div>
+                                        </div>
+                                    </li>
+                                    <li class="showroom-item col-4">
+                                        <div class="showroom-content">
+                                            <h4 class="showroom-content-title">
+                                                Washington
+                                            </h4>
+                                            <div class="showroom-content-description">
+                                                <p>Address: Puffin street 12345 Puffinville <br>
+                                                    Phone: (123) 4567889 <br>
+                                                    Opening hours: 8:00 to 21:00 (all days)</p>
+                                                <div class="btn-color"><a href="#">View on map </a> <i
+                                                        class="fa-solid fa-angles-right fa-2xs"></i></div>
+                                            </div>
+                                        </div>
+                                    </li>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-3 col-xs-12">
+                            <h3 class="h3-title">follow us on facebook</h3>
+                        </div>
+                    </div>
+                    <div class="footer-top">
+                        <div class="row">
+                            <div class="footer-top-item col-md-3">
+                                <h3 class="footer-top-item-title">Information</h3>
+                                <ul class="footer-sub-menu">
+                                    <li class="footer-sub-menu-item"><a href="">Terms and conditions of use</a></li>
+                                    <li class="footer-sub-menu-item"><a href="">Prices drop</a></li>
+                                    <li class="footer-sub-menu-item"><a href="">New products</a></li>
+                                    <li class="footer-sub-menu-item"><a href="">Best sales</a> </li>
+                                    <li class="footer-sub-menu-item"><a href="">Stores</a></li>
+                                </ul>
+                            </div>
+                            <div class="footer-top-item col-md-3">
+                                <h3 class="footer-top-item-title">My Account</h3>
+                                <ul class="footer-sub-menu">
+                                    <li class="footer-sub-menu-item"><a href="">Personal info</a></li>
+                                    <li class="footer-sub-menu-item"><a href="">Orders</a></li>
+                                    <li class="footer-sub-menu-item"><a href="">Credit slips</a></li>
+                                    <li class="footer-sub-menu-item"><a href="">Addresses</a> </li>
+                                </ul>
+                            </div>
+                            <div class="footer-top-item col-md-3">
+                                <h3 class="footer-top-item-title">Company</h3>
+                                <ul class="footer-sub-menu">
+                                    <li class="footer-sub-menu-item"><a href="">About us</a></li>
+                                    <li class="footer-sub-menu-item"><a href="">Contact us</a></li>
+                                    <li class="footer-sub-menu-item"><a href="">Delivery</a></li>
+                                    <li class="footer-sub-menu-item"><a href="">Privacy</a> </li>
+                                </ul>
+                            </div>
+                            <div class="footer-top-item col-md-3" style="margin-bottom: 10px;">
+                                <h3 class="footer-top-item-title">Newsletter</h3>
+                                <div class="newsletter-content">
+                                    <form action="">
+                                        <div class="row">
+                                            <p class="newsletter-content-text">Stay up to date with our company news,
+                                                new productsand exclusive offers
+                                            </p>
+                                            <div class="enter-your-email">
+                                                <div class="newsletter-submit">
+                                                    <input type="submit" class="btn btn-primary pull-xs-right">
+                                                </div>
+                                                <div class="input-wrapper">
+                                                    <input id="email" type="text" placeholder="Enter your email...">
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                            <div class="footer-top-item col-md-3"></div>
+                            <div class="footer-top-item col-md-3"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+</body>
+<script src="./assets/js/bootstrap.bundle.min.css">
+
+</script>
+
+</html>
