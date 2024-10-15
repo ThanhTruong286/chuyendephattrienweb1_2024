eab1d3355dc662db0796849cc12aab816fe27594
ThanhTruong286 <caot43069@gmail.com>
2024-10-15 19:15:38 +0700

    complete lab 4

--- front/3220/index.php
+++ front/3220/index.php
@@ -17,12 +17,14 @@ $less->compileFile('./assets/less/3220.less', './assets/css/3220.css');
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="./assets/css/3220.css">
 </head>
+
 <body>
     <div id="qodef-page-inner" class="qodef-content-grid">
         <main id="qodef-page-content" class="qodef-grid qodef-layout--template qodef-gutter--large">
@@ -34,7 +36,8 @@ $less->compileFile('./assets/less/3220.less', './assets/css/3220.css');
                                 <div id="bbp-user-avatar">
                                     <span class="vcard">
                                         <a href="#" class="url fn n">
-                                            <img id="avatar" height="150" width="150" src="./assets/images/avatar.jpg" alt="">
+                                            <img id="avatar" height="150" width="150" src="./assets/images/avatar.jpg"
+                                                alt="">
                                         </a>
                                     </span>
                                 </div>
@@ -42,7 +45,7 @@ $less->compileFile('./assets/less/3220.less', './assets/css/3220.css');
                                     <ul>
                                         <li class="current">
                                             <span class="vcard bbp-user-profile-link">
-                                                <a href="#" class="url fn n">
+                                                <a id="current-link" href="#" class="url fn n">
                                                     Profile
                                                 </a>
                                             </span>
@@ -50,35 +53,52 @@ $less->compileFile('./assets/less/3220.less', './assets/css/3220.css');
                                         <li>
                                             <span class="bbp-user-topics-created-link">
                                                 <a href="#">
-                                                Topics Started
+                                                    Topics Started
                                                 </a>
                                             </span>
                                         </li>
                                         <li>
-                                        <span class="bbp-user-topics-created-link">
+                                            <span class="bbp-user-topics-created-link">
                                                 <a href="#">
-                                                Replies Created
+                                                    Replies Created
                                                 </a>
-                                        </span>
+                                            </span>
                                         </li>
                                         <li>
-                                        <span class="bbp-user-topics-created-link">
+                                            <span class="bbp-user-topics-created-link">
                                                 <a href="#">
-                                                Engagements
+                                                    Engagements
                                                 </a>
-                                        </span>
+                                            </span>
                                         </li>
                                         <li>
-                                        <span class="bbp-user-topics-created-link">
+                                            <span class="bbp-user-topics-created-link">
                                                 <a href="#">
-                                                Favorites
+                                                    Favorites
                                                 </a>
-                                        </span>
+                                            </span>
                                         </li>
                                     </ul>
                                 </div>
                             </div>
-                            <div id="bbp-user-body"></div>
+                            <div id="bbp-user-body">
+                                <div class="bbp-user-profile">
+                                    <h2>@anthony-johnson</h2>
+                                    <div class="bbp-user-section">
+                                        <h3>Profile</h3>
+                                        <p class="bbp-user-forum-role">Registered: 3 years, 11 months ago</p>
+                                        <p class="bbp-user-description">Lorem ipsum dolor sit amet, consectetur
+                                            adipisicing elit, sed do eius tempor incididunt ut labore et dolore magna.
+                                        </p>
+                                        <hr>
+                                        <h3>Forums</h3>
+                                        <p class="bbp-user-last-activity">Last Activity: 3 years, 10 months ago</p>
+                                        <p class="bbp-user-topic-count">Topics Started: 0</p>
+                                        <p class="bbp-user-reply-count">Replies Created: 4</p>
+                                        <p class="bbp-user-forum-role">Forum Role: Participant</p>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -86,4 +106,5 @@ $less->compileFile('./assets/less/3220.less', './assets/css/3220.css');
         </main>
     </div>
 </body>
+
 </html>
